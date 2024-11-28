57b4f82755f40cdca4ae2f2d5b6b837c8685837b
TheBodger <41484656+TheBodger@users.noreply.github.com>
2024-11-28 21:35:43 +0000

    Rewrite Dom Gen

--- MMM-TomTomTrafficMaps.js
+++ MMM-TomTomTrafficMaps.js
@@ -1,7 +1,11 @@
-Module.register("MMM-TomTomTrafficMaps",{
+Module.register("MMM-TomTomTrafficMaps", {
+
+	//cdn/6.x/6.25.1
+
+	//https://developer.tomtom.com/maps-sdk-web-js/functional-examples
 
 	defaults: {
-		requiresVersion: "2.8.0",  //Developped and tested with this version.
+		requiresVersion: "6.25.1",  //Developped and tested with this version.
 		key: "",
 		lang: config.language,
 		height: "75vh",
@@ -13,7 +17,7 @@ Module.register("MMM-TomTomTrafficMaps",{
 		showIncidents: true,
 		showTraffic: true,
 		zoom: 11,
-		TTVersion: "5.47.0" //Internal solution to quickly change version of TomTom API to a new version.
+		TTVersion: "6.25.1" //Internal solution to quickly change version of TomTom API to a new version.
 	},
 
 		//tomtom://vector/1/basic-main
@@ -24,18 +28,19 @@ Module.register("MMM-TomTomTrafficMaps",{
 		//tomtom://vector/1/labels-night
 		//tomtom://vector/1/basic-light
 		
-	getStyles: function() {
+	getStyles: function () {
+		//https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.25.1/maps/maps.css
+		//"https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/" +  this.config.TTVersion + "/maps/css-styles/traffic-incidents.css"
 
 		return [
-			"https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" +  this.config.TTVersion + "/maps/maps.css",
-			"https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" +  this.config.TTVersion + "/maps/css-styles/traffic-incidents.css"
+			"https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/" + this.config.TTVersion + "/maps/maps.css"
 		]
 
 	},
 	// Define required scripts.
 	getScripts: function () {
 		return [
-			"https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" + this.config.TTVersion + "/maps/maps-web.min.js"
+			"https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/" + this.config.TTVersion + "/maps/maps-web.min.js"
 		];
 	},
 
@@ -55,27 +60,8 @@ Module.register("MMM-TomTomTrafficMaps",{
 
 			let script = document.createElement("script")
 			script.type = "text/javascript";
-			script.innerHTML += "tt.setProductInfo('MagicMirror TomTom Traffic & Incidents', '1.0'); ";
-			script.innerHTML += `let map = new tt.map({key: '${this.config.key}',container: 'ttmap',center: [${this.config.lng},${this.config.lat}],`;
-			script.innerHTML += `zoom: ${this.config.zoom},style: 'tomtom://vector/1/${this.config.mapStyle}',language: '${this.config.lang}',interactive: false});`;
-			//script.innerHTML += `map.addControl(new tt.NavigationControl());`;
-			script.innerHTML += `var flowconfig = { key: '${this.config.key}',style: 'tomtom://vector/1/${this.config.trafficStyle}',refresh: ${this.config.refresh}};`;
-			script.innerHTML += `var incconfig = { key: '${this.config.key}', incidentTiles: { style: 'tomtom://vector/1/${this.config.incidentStyle}' }, incidentDetails: { style: '${this.config.incidentStyle}' } };`;
-			script.innerHTML += `map.on('load', function () {`;
 
-			if (!this.config.showIncidents) { }
-			else
-			{
-				script.innerHTML += "map.addTier(new tt.TrafficFlowTilesTier(flowconfig));";
-			}
-
-			if (!this.config.showTraffic) { }
-			else
-			{
-				script.innerHTML += "map.addTier(new tt.TrafficIncidentTier(incconfig));";
-			}
-
-			script.innerHTML += "});"; //alert('loaded maps');
+			script.innerHTML += `var map = tt.map( {key:'${this.config.key}', container: 'ttmap', center: [${this.config.lng},${this.config.lat}], zoom: ${this.config.zoom}, stylesVisibility: {trafficIncidents: ${this.config.showIncidents},trafficFlow: ${this.config.showTraffic}} } );`
 
 			document.body.appendChild(script);
 
@@ -83,7 +69,7 @@ Module.register("MMM-TomTomTrafficMaps",{
 	},
 
 	getDom: function () {
-		// use ttmpa instead of map so if other module uses the map class or div it doesnt impact us here
+		// use ttmap instead of map so if other module uses the map class or div it doesnt impact us here
 		let wrapper = document.createElement("div");
 		wrapper.setAttribute("id", "ttmap");
 		wrapper.setAttribute("class", "ttmap");
